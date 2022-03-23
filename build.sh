d3f6e1999a2b18afb5800192984be56813ebbdc5
islandcontroller <13700483+islandcontroller@users.noreply.github.com>
2022-03-23 06:24:59 +0000

    Added md5 and sha256 to build script

--- build.sh
+++ build.sh
@@ -10,3 +10,5 @@ docker run --rm -it \
             cp -r /app/LoadscreenMute/* . &&
             dotnet build &&
             cp /tmp/Outputs/LoadscreenMute.dll /app/Outputs'
+echo "md5sum     $(md5sum ./Outputs/LoadscreenMute.dll | awk '{ print $1 }')"
+echo "sha256sum  $(sha256sum ./Outputs/LoadscreenMute.dll | awk '{ print $1 }')"
